3c69badb4afa265eec08063c221068d8eedaecc6
Sinister Light <me@sinisterlight.com>
2013-12-24 01:47:12 +0530

    [Shobhit] fixing torqbox install

--- config/software/torqbox.rb
+++ config/software/torqbox.rb
@@ -4,8 +4,15 @@ name "torqbox"
 dependency "jruby"
 
 env = {
-        "JAVA_HOME" => "#{install_dir}/embedded/jre/"
-      }
+  "JAVA_HOME"       => "#{install_dir}/embedded/jre/",
+  "RUBYOPT"         => nil,
+  "BUNDLE_BIN_PATH" => nil,
+  "BUNDLE_GEMFILE"  => nil,
+  "GEM_PATH"        => nil,
+  "GEM_HOME"        => nil,
+}
+
+
 
 build do
   command "#{install_dir}/embedded/jruby/bin/jruby -S gem install bundler", "env" => env
