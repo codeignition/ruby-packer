b1b7fe213c6971f28ae5fb42efb965cfb39694fb
sharmavipul <vipulsharma2190@gmail.com>
2015-03-12 19:15:47 +0530

    fixed typo

--- config/software/cacerts.rb
+++ config/software/cacerts.rb
@@ -19,7 +19,7 @@ name "cacerts"
 version "2014.01.28" # date of the file is in a comment at the start, or in the changelog
 
 source :url => "http://curl.haxx.se/ca/cacert.pem",
-       :md5 => ENV['CACERTS-MD5']
+       :md5 => ENV['CACERTS_MD5']
 
 relative_path "cacerts-#{version}"
 
